0517efbb919cf487791127284397cd40a875b084
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-23 11:19:36 +0200

    list is showing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import employees from './data/employees.json';
 import './style.css'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h1>JSON Site</h1>
-<p>testing</p>
+<p>There are ${employees.length} employees.</p>
+<ul>
+  ${employees.map(employee => {
+    return `<li>${employee.firstName} <strong>${employee.lastName}</strong> - ${employee.address.city}</li>`
+  }).join('')}
+</ul>
 `
