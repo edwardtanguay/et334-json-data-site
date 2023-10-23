7864c4eb7c4224486ca38e001aaf77dd8efc5fbf
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-23 11:20:48 +0200

    is sorted

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import employees from './data/employees.json';
 import './style.css'
 
+employees.sort((a, b) => a.lastName > b.lastName ? 1 : -1);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h1>JSON Site</h1>
