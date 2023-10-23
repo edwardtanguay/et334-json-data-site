8e5b1cc9d9499cb3dcbc53b9acef9d26aa8a6960
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-23 11:49:18 +0200

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import employees from './data/employees.json';
 import './style.css'
 
+interface IEmployee {
+  employeeID: number;
+  firstName: string;
+  lastName: string;
+  reportsTo: number | "NULL";
+}
+
 employees.sort((a, b) => a.lastName > b.lastName ? 1 : -1);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -10,17 +17,22 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   ${employees.map(employee => {
   return `<li>ID=${employee.employeeID} - ${employee.firstName} <strong>${employee.lastName}</strong> - ${employee.address.city}
    <ul>
-    <li>reports to: <span style="color: red">${getReportsToName(employee)}</span></li>
+    <li>reports to: <span style="color: red">${getReportsToName(employee as IEmployee)}</span></li>
    </ul>
     </li>`
 }).join('')}
 </ul>
 `
 
-function getReportsToName(employee) {
+function getReportsToName(employee: IEmployee) {
   console.log(employee.reportsTo);
   if (employee.reportsTo !== 'NULL') {
-    return 'nnn'; // employees.find(m => m.employeeID === employee.reportsTo).firstName
+    const manager = employees.find(m => m.employeeID === employee.reportsTo);
+    if (manager) {
+      return manager.firstName;
+    } else {
+      return 'UNKNOWN ID: ' + employee.reportsTo;
+    }
   } else {
     return 'nobody';
   }
